6944da2843058e9076bd7b0198acf4dc2931c56c
Bogdanov Ivan <ibogdanov@rsspnet.ru>
2018-04-20 23:00:17 +0300

    Change ncurses

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <unistd.h>
-#include <ncurses.h>
+#include <stdlib.h>
 
 
 using namespace std;
@@ -43,42 +43,6 @@ int kbhit(void)
   return 0;
 }
 
-<<<<<<< HEAD
-=======
-#include <stdio.h>
-#include <termios.h>
-#include <unistd.h>
-#include <fcntl.h>
- 
-int kbhit(void)
-{
-  struct termios oldt, newt;
-  int ch;
-  int oldf;
- 
-  tcgetattr(STDIN_FILENO, &oldt);
-  newt = oldt;
-  newt.c_lflag &= ~(ICANON | ECHO);
-  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
- 
-  ch = getchar();
- 
-  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-  fcntl(STDIN_FILENO, F_SETFL, oldf);
- 
-  if(ch != EOF)
-  {
-    ungetc(ch, stdin);
-    return 1;
-  }
- 
-  return 0;
-}
-
-
->>>>>>> parent of 10d31ac... Revert "no message"
 enum eDirection
 {
     STOP = 0,
