03bcb50044beb81005f24e98d87c12d04fe78933
Bogdanov Ivan <ibogdanov@rsspnet.ru>
2018-04-20 15:57:32 +0300

    Add logic
    Add view score
    Add eat fruit

--- main.cpp
+++ main.cpp
@@ -1,14 +1,45 @@
 #include <iostream>
 #include <unistd.h>
-#include <ncurses.h>
-
+#include <stdlib.h>
 
 using namespace std;
 
 bool gameOver;
 int const width = 40;
 int const height = 20;
-int x, y, fruitX, fruitY, score;
+int x, y, fruitX, fruitY, score = 0;
+
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+ 
+int kbhit(void)
+{
+  struct termios oldt, newt;
+  int ch;
+  int oldf;
+ 
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+ 
+  ch = getchar();
+ 
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  fcntl(STDIN_FILENO, F_SETFL, oldf);
+ 
+  if(ch != EOF)
+  {
+    ungetc(ch, stdin);
+    return 1;
+  }
+ 
+  return 0;
+}
 
 enum eDirection
 {
@@ -24,8 +55,8 @@ void start()
 {
     gameOver = false;
     dir = STOP;
-    x = width / 2 - 1;
-    y = height / 2 - 1;
+    x = width / 2;
+    y = height / 2;
     fruitX = rand() % width;
     fruitY = rand() % height;
 }
@@ -40,12 +71,16 @@ void draw()
     {
         for(int j = 0; j < width; ++j)
         {
+
             if(j == 0 || j == width - 1)
                 cout << "#";
+
             if(i == y && j == x)
                 cout << "0";
+
             else if(i == fruitY && j == fruitX)
                 cout << "F";
+
             cout << " ";
         }
         cout << endl;
@@ -55,21 +90,62 @@ void draw()
         cout << "#";
     cout << endl;
 
-//    sleep(1);
+    cout << "Score: " << score << endl;
+
+       sleep(1);
 }
 void input()
 {
     if(kbhit())
-    switch(getch())
+    switch(getchar())
     {
-        if(getch() == 'a')
+    case 'a':
+        dir = LEFT;
+        break;
+    case 'd':
+        dir = RIGHT;
+        break;
+    case 'w':
+        dir = UP;
+        break;
+    case 's':
+        dir = DOWN;
+        break;
+    case 'x':
+        gameOver = true;
+        break;
+
     }
 }
 void logic()
 {
+    switch(dir)
+    {
 
+    case LEFT:
+        x--;
+        break;
+    case RIGHT:
+        x++;
+        break;
+    case UP:
+        y--;
+        break;
+    case DOWN:
+        y++;
+        break;
+    }
+
+    if ( x > width || x < 0 || y > height || y < 0)
+        gameOver = true;
+    if ( x == fruitX && y == fruitY)
+    {
+        score++;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+
+    }
 }
-
 
 int main()
 {
