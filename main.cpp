380bc431aa8563726d71463bf2d9415697f46377
Bogdanov Ivan <ibogdanov@rsspnet.ru>
2018-04-20 23:05:15 +0300

    All fix and change.
    This code!

--- main.cpp
+++ main.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
 #include <unistd.h>
 #include <stdlib.h>
-<<<<<<< HEAD
-
-=======
->>>>>>> 10d31ac84f4f9311e3690aa082c9599b788d8d7c
 
 using namespace std;
 
@@ -45,41 +41,6 @@ int kbhit(void)
   return 0;
 }
 
-<<<<<<< HEAD
-=======
-#include <stdio.h>
-#include <termios.h>
-#include <unistd.h>
-#include <fcntl.h>
- 
-int kbhit(void)
-{
-  struct termios oldt, newt;
-  int ch;
-  int oldf;
- 
-  tcgetattr(STDIN_FILENO, &oldt);
-  newt = oldt;
-  newt.c_lflag &= ~(ICANON | ECHO);
-  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
- 
-  ch = getchar();
- 
-  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-  fcntl(STDIN_FILENO, F_SETFL, oldf);
- 
-  if(ch != EOF)
-  {
-    ungetc(ch, stdin);
-    return 1;
-  }
- 
-  return 0;
-}
-
->>>>>>> 10d31ac84f4f9311e3690aa082c9599b788d8d7c
 enum eDirection
 {
     STOP = 0,
